115c8bd4e1afaa7d2627178cb7bbffd263ac4c11
pat-in-a-hat <wpatrickwork@gmail.com>
2022-10-27 16:01:42 -0600

    week 13 first commit after resolving react load errors

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Form() {
+    return (
+        <div className='flex-container'>
+            <div>
+                <h3 class = "login-title">Log In</h3>
+                <form>
+                    <label for='fname'>Username</label><br>
+                    <
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Form;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+    <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Clients</li>
+        <li>About Us</li>
+    </ul>
+    )
+}
+
+export default NavBar;
