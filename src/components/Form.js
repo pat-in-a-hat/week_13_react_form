e84958f56b69f738cc0d91c27d0dc30f6ca710f6
pat-in-a-hat <wpatrickwork@gmail.com>
2022-10-27 19:52:45 -0600

    added styling, form requirements for this week

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 function Form() {
     return (
-        <div className='flex-container'>
-            <div>
-                <h3 class = "login-title">Log In</h3>
-                <form>
-                    <label for='fname'>Username</label><br>
-                    <
+        <div className='grid-container'>
+            <div className='grid-item login'>
+                <h3 className='login-title'>Log In</h3>
+                <div>
+                <form id='form'>
+                    <label for='fname'>Username</label><br></br>
+                    <input type='text' id='fname'></input><br></br><br></br>
+                    <label for='pword'>Password</label><br></br>
+                    <input type='password' id='pword'></input><br></br><br></br>
                 </form>
+                <button type='submit' form='form' value='submit'>Submit</button><br></br>
+                </div>
             </div>
         </div>
     )
